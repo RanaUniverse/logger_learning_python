afb9049796f9dedb63fc857289ce6c4d1503c297
RanaUniverse <RanaUniverse321@gmail.com>
2025-12-23 14:12:25 +0530

    logger learning from beginning

---
+++ main.py
@@ -0,0 +1,10 @@
+'''
+This is for learning how the logger works
+'''
+
+first_name = "Rana"
+last_name = "Universe"
+
+full_name = first_name + " " + last_name
+
+from utils.custom_logger import logging
---
+++ utils/custom_logger.py
@@ -0,0 +1,11 @@
+import logging
+
+# abc = logging.warning("Remain Calm!")
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug("This is a debug msg")
+logging.info("Info of the system is here")
+
+logging.warning("This is for warning")
+logging.error("Somethigns error happesn in my system")
+logging.critical("This is the critical msg")
