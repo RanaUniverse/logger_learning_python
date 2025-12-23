1840a1bd2983b2bdde3a3745e9b8b2f7ded01afa
RanaUniverse <RanaUniverse321@gmail.com>
2025-12-23 14:46:01 +0530

    now the utc +0 timefort will be used now in the message in the logging

--- utils/custom_logger.py
+++ utils/custom_logger.py
@@ -1,7 +1,22 @@
 import logging
 
+import time
+
+logging.Formatter.converter = time.gmtime
+
 # abc = logging.warning("Remain Calm!")
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(
+#     level=logging.DEBUG,
+#     format="%(levelname)s:%(name)s:%(message)s",
+# )
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    # format="{levelname}:{name}:{message}", style="{",
+    format="{asctime} - {levelname} - {message}",
+    style="{",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 logging.debug("This is a debug msg")
 logging.info("Info of the system is here")
